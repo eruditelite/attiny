67b8f4e0c014b713c4e8f4e460646b651533897b
John Jacques <eruditelite@gmail.com>
2018-05-28 17:49:16 -0500

    Add the example84/square Example
    Signed-off-by: John Jacques <eruditelite@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,5 @@
 AVRTARGET=84 make $@
 AVRTARGET=84 make -C examples84/blink $@
 AVRTARGET=85 make -C examples85/blink $@
+AVRTARGET=84 make -C examples84/square $@
 AVRTARGET=85 make -C examples85/square $@
