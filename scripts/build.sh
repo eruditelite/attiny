286462e3b4a0f429f5b55ed30bc7f58ab8db4ec1
John Jacques <eruditelite@gmail.com>
2018-06-08 12:16:53 -0500

    A Cleaned Up Wave Example for ATTiny85
    Signed-off-by: John Jacques <eruditelite@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,4 @@ AVRTARGET=84 make -C examples84/blink $@
 AVRTARGET=85 make -C examples85/blink $@
 AVRTARGET=84 make -C examples84/square $@
 AVRTARGET=85 make -C examples85/square $@
+AVRTARGET=85 make -C examples85/wave $@
