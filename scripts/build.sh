d71430f829752d43dff4a1a55267d64b7568e375
John Jacques <eruditelite@gmail.com>
2018-06-15 17:58:23 -0500

    Update the Directories in scripts/build.sh
    Don't try to build the top directory, it is now just
    a template!
    
    Add examples85/adc and example85/activity.
    
    Signed-off-by: John Jacques <eruditelite@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-AVRTARGET=84 make $@
 AVRTARGET=84 make -C examples84/blink $@
 AVRTARGET=85 make -C examples85/blink $@
 AVRTARGET=84 make -C examples84/square $@
 AVRTARGET=85 make -C examples85/square $@
 AVRTARGET=85 make -C examples85/wave $@
+AVRTARGET=85 make -C examples85/adc $@
+AVRTARGET=85 make -C examples85/activity $@
