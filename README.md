765233a9420010bff10ba3794ec3f093566c8430
John Jacques <eruditelite@gmail.com>
2018-06-18 11:05:41 -0500

    Update the "square" and "wave" Examples in 85
    Signed-off-by: John Jacques <eruditelite@gmail.com>

--- README.md
+++ README.md
@@ -24,6 +24,14 @@ include from each to +3.3V.
 
 BCM 22 is used as a reset, connected to both ATTinys.
 
+In some cases, multiple pins from the Raspberry Pi connect to a single
+pin on the ATTiny.  All pins related to the function not being used
+(I2C or SPI) should be tristated.  In this document, the BCM numers
+will be used and the 'gpio' command will be used with the '-g' option.
+Details are available in makefile.attiny84 and makefile.attiny85.
+Both of those makefiles include macros to switch the Raspberry
+Pi/ATTiny connection to SPI mode or to I2C mode.
+
 ## Examples ##
 
 ### ATTiny 84 Simple ###
@@ -40,7 +48,29 @@ BCM 22 is used as a reset, connected to both ATTinys.
 
 ### ATTiny 85 Square ###
 
+Square wave output on PB1.  OCR1A is the "percentage on" (0...255);
+the initial value is 127.  Use a low pass filter to smooth the output.
+Resistor and capacitor values will depend on the load etc.
+
+![Low Pass](images/lowpassfordc.png)
+
 ### ATTiny 85 Wave ###
+
+This example uses timer/counter 1 for PWM output on PB4 and
+timer/counter 0 as a trigger to change the sample.  The samples are
+stored in eeprom.  An example of generating sample sets is provited by
+wavegen.c (after compiling, see ./wavegen -h for a description).
+Writing the sample to eeprom is handled by the 'eeprom' target in
+makefile.attiny.  First a binary is created by wavegen, then the make
+target converts the binary to Intel hex and writes it to the eeprom
+using avrdude.
+
+As the interrupts from timer/counter 0 are fairly fast, I2C is not
+supported.
+
+Use a low pass filter on the output as follows.
+
+![Low Pass](images/lowpassforwave.png)
 
 ### ATTiny 85 ADC ###
 
--- examples85/wave/main.c
+++ examples85/wave/main.c
@@ -8,60 +8,9 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-#include "tick.h"
-#include "i2c.h"
-
-#define I2C_ADDRESS 5
-
-#define MAGIC   0xbacd
-#define PROJECT 0x0004
-#define VERSION 0x0001
-
 #define WAVE_SIZE 0
 #define SAMPLE_RATE 2
 #define WAVE_TABLE 3
-
-/*
-  ------------------------------------------------------------------------------
-  i2c_callback
-*/
-
-void
-i2c_callback(uint8_t ibl, const uint8_t *ib, uint8_t *obl, uint8_t *ob)
-{
-	uint8_t ibi;
-	uint8_t obi = 0;
-
-	cli();
-
-	for (ibi = 0; ibi < ibl;) {
-		switch (ib[ibi++]) {
-		case 0x00:
-			/* Return the magic number, 0xbacd. */
-			ob[obi++] = MAGIC & 0xff;
-			ob[obi++] = (MAGIC & 0xff00) >> 8;
-			break;
-		case 0x01:
-			/* Return the project. */
-			ob[obi++] = PROJECT & 0xff;
-			ob[obi++] = (PROJECT & 0xff00) >> 8;
-			break;
-		case 0x02:
-			/* Return the version. */
-			ob[obi++] = VERSION & 0xff;
-			ob[obi++] = (VERSION & 0xff00) >> 8;
-			break;
-		default:
-			break;
-		}
-	}
-
-	*obl = obi;
-
-	sei();
-
-	return;
-}
 
 /*
   ------------------------------------------------------------------------------
@@ -96,31 +45,22 @@ main(void)
 	OCR1B = 0;
 	OCR1C = 255;
 
+	TIMSK = _BV(OCIE0A);
+
 	/* timer0... interrupt to output samples */
 
 	TCCR0A = _BV(WGM00);
 	TCCR0B = _BV(WGM02) | _BV(CS00);
 
-	/* pin change interrupt on PB1 for I2C */
-
-	GIMSK = _BV(PCIE);
-	PCMSK = _BV(PCINT1);
-
 	/*
-	  Start the Tick
+	  Enable Interrupts
 	*/
 
-	start_tick(0);
-
-	/*
-	  Start I2C - Loop for I2C Messages
-	*/
-
-	start_i2c(I2C_ADDRESS, i2c_callback);
+	sei();
 
 	/* Then... */
 	for (;;)
-		usi_twi_check();
+		;
 
 	return 0;
 }
@@ -137,11 +77,6 @@ ISR(TIMER0_COMPA_vect)
 
 ISR(PCINT0_vect)
 {
-	if (0 == (PINB & _BV(PB1))) {
-		TIMSK &= ~_BV(OCIE0A);
-		OCR1B = 0;
-	} else {
-		OCR0A = eeprom_read_byte((uint8_t *)SAMPLE_RATE);
-		TIMSK |= _BV(OCIE0A);
-	}
+	OCR0A = eeprom_read_byte((uint8_t *)SAMPLE_RATE);
+	TIMSK |= _BV(OCIE0A);
 }
