c683702e6e2653d68a58a03bf52a6e85f387d9cb
John Jacques <eruditelite@gmail.com>
2019-04-09 09:57:46 -0500

    Add a Delay in the Reset Sequence
    Signed-off-by: John Jacques <eruditelite@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,14 +14,20 @@ gpio -g mode 11 in
 
 gpio -g mode 22 out
 gpio -g mode 23 out
-gpio -g write 23 0
-gpio -g write 22 1
-gpio -g write 22 0
-gpio -g write 22 1
 
 gpio -g mode 24 out
 gpio -g mode 27 out
+
+gpio -g write 23 0
+gpio -g write 22 1
+sleep 1
+gpio -g write 22 0
+sleep 1
+gpio -g write 22 1
+
 gpio -g write 24 0
 gpio -g write 27 1
+sleep 1
 gpio -g write 27 0
+sleep 1
 gpio -g write 27 1
